15444e2c2dd0d24c4c29bad10d5d3687643efc3f
Alexandr <santer4249@mail.ru>
2021-10-06 23:31:14 +0300

    Мой второй вариант бота, команды вводятся с кнопок и добавлена функционал конвертера, но еще не доработан.

---
+++ bot/api_belarusbank.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def get_exchange_rates(currencies: list, direstion: str, city: str) -> str:
+    result = ''
+    for currency in currencies:
+        s = requests.Session()
+        r = s.get(f'https://belarusbank.by/api/kursExchange?city={city.title()}')
+        r = json.loads(r.text)
+        result += f'{currency.upper()}: {r[0][f"{currency.upper()}_{direstion.lower()}"]} \n'
+    return result
+
+if __name__ == '__main__':
+    print(get_exchange_rates(['usd', 'eur', 'rub'], 'out', 'минск'))
+
+
+def get_exchange_rates_convers(currencies: list, direstion: str, city: str) -> str:
+    result = ''
+    for currency in currencies:
+        s = requests.Session()
+        r = s.get(f'https://belarusbank.by/api/kursExchange?city={city.title()}')
+        r = json.loads(r.text)
+        result += f'{currency.upper()}: {r[0][f"{currency.upper()}_{direstion.lower()}"]} \n'
+    return result
+
+if __name__ == '__main__':
+    print(get_exchange_rates_convers(['usd_eur', 'usd_rub', 'rub_eur'], 'out', 'минск'))
+    print(get_exchange_rates_convers(['usd_eur', 'usd_rub', 'rub_eur'], 'in', 'минск'))
